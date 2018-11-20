0a0db9a32b5b74cba201501c8e83c57d06aa8163
kparf-itechart <kiryl.parfiankou@itechart-group.com>
2018-11-20 15:35:14 +0300

    Registracton Switch Page was added

--- src/bundles/common/components/Content/Content.js
+++ src/bundles/common/components/Content/Content.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router'
+
 import styles from './Content.module.css';
+import RegistrationSwitcherPage from '../RegistrationSwitcherPage';
 
 function Content() {
     return (
         <main className={styles.Content}>
-            Content
+            <Switch>
+                <Route path="/registration" component={RegistrationSwitcherPage}/>
+            </Switch>
         </main>
     )
 }
---
+++ src/bundles/common/components/RegistrationSwitcherPage/RegistrationSwitcherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RegistrationSwitcherPage.module.css';
+
+function RegistrationSwitcherPage() {
+    return (
+        <div className={styles.RegistrationSwitcherPage}>
+            Registration Switcher
+        </div>
+    )
+}
+
+export default RegistrationSwitcherPage;
