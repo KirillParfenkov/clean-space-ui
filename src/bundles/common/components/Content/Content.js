855ab78d28d303d6b455340ca56f52cc9b515734
kparf-itechart <kiryl.parfiankou@itechart-group.com>
2018-11-20 15:49:27 +0300

    Booking Page was added

--- src/bundles/common/components/Content/Content.js
+++ src/bundles/common/components/Content/Content.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router'
+import RegistrationSwitcherPage from '../RegistrationSwitcherPage';
+import ServicesPage from '../ServicesPage';
 
 import styles from './Content.module.css';
-import RegistrationSwitcherPage from '../RegistrationSwitcherPage';
 
 function Content() {
     return (
         <main className={styles.Content}>
             <Switch>
                 <Route path="/registration" component={RegistrationSwitcherPage}/>
+                <Route path="/services" component={ServicesPage}/>
             </Switch>
         </main>
     )
---
+++ src/bundles/common/components/ServicesPage/ServicesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ServicesPage.module.css';
+
+function ServicesPage() {
+    return (
+        <div className={styles.ServicesPage}>
+            Services
+        </div>
+    )
+}
+
+export default ServicesPage;
