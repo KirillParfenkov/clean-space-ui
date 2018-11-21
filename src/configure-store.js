b2fc9756d57b05ae6bdd081ee734a3ac623d0b0c
kparf-itechart <kiryl.parfiankou@itechart-group.com>
2018-11-21 15:36:58 +0300

    Saga and redux-action ware added

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger';
 import createReducer from './create-reducer';
 
 const loggerMiddleware = createLogger();
+const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(preLoadedState = {}) {
@@ -12,6 +14,7 @@ export default function configureStore(preLoadedState = {}) {
         composeEnhancers(
             applyMiddleware(
                 loggerMiddleware,
+                sagaMiddleware,
             )
         )
     )
