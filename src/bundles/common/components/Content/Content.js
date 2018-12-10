71c274feac361281500f318e10e5c12afacbccd0
kparf-itechart <kiryl.parfiankou@itechart-group.com>
2018-12-10 20:37:39 +0300

    Added Service Serach Page blueprint

--- src/bundles/common/components/Content/Content.js
+++ src/bundles/common/components/Content/Content.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router'
-import RegistrationSwitcherPage from '../RegistrationSwitcherPage';
-import BookingPage from '../../../client/components/BookingPage';
-import RegisterClientPage from '../../../client/components/RegisterClientPage';
-import RegisterCompanyPage from "../../../service/pages/RegisterCompanyPage";
-import ServicesPage from '../ServicesPage';
+import RegistrationSwitcherPage from 'bundles/common/components/RegistrationSwitcherPage';
+import BookingPage from 'bundles/client/components/BookingPage';
+import RegisterClientPage from 'bundles/client/components/RegisterClientPage';
+import RegisterCompanyPage from 'bundles/service/pages/RegisterCompanyPage';
+import ServiceSearchPage from 'bundles/service/pages/ServiceSearchPage';
 
 import styles from './Content.module.css';
 import LoginPage from '../../pages/LoginPage';
@@ -17,7 +17,7 @@ function Content() {
                 <Route path="/registration/client" component={RegisterClientPage}/>
                 <Route path="/registration/company" component={RegisterCompanyPage}/>
                 <Route path="/registration" component={RegistrationSwitcherPage}/>
-                <Route path="/services" component={ServicesPage}/>
+                <Route path="/services" component={ServiceSearchPage}/>
                 <Route path="/login" component={LoginPage}/>
             </Switch>
         </main>
---
+++ src/bundles/service/pages/ServiceSearchPage/ServiceSearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasePage from 'bundles/common/components/BasePage';
+
+function ServiceSearchPage() {
+    return (
+        <BasePage>
+            Service Search Page!
+        </BasePage>
+    )
+}
+
+export default ServiceSearchPage;
